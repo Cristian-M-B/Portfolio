ab352725d9a2e2facf9c071038dc6151292a6cca
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-21 22:38:30 -0300

    load certification

---
+++ src/assets/certifications.js
@@ -0,0 +1,11 @@
+import henry from './images/certifications/henry.png';
+
+const images = [
+    {
+        name: 'Henry Certification',
+        image: henry,
+        link: 'https://certificates.soyhenry.com/cert?id=69fcab18-ec7e-431a-a06c-d2de58ec9c1e'
+    }
+]
+
+export default images;
