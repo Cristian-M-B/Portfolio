010bd08493f4478d8b563918ec49e7eb5b8f8d24
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-21 22:43:12 -0300

    component Technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Nav from './Nav.jsx';
 import Footer from './Footer.jsx';
+import SmallCard from './SmallCard.jsx';
+import technologies from '../assets/technologies.js';
 import '../styles/Technologies.scss';
 
 export default function Technologies() {
@@ -8,7 +10,11 @@ export default function Technologies() {
         <>
             <Nav />
             <div className='technologies'>
-                Technologies
+                <div className='logos'>
+                    {technologies?.map(tech => {
+                        return <SmallCard image={tech?.image} name={tech?.name} />
+                    })}
+                </div>
             </div>
             <Footer />
         </>
