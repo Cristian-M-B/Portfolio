96894f5c19dffd86e531b760f367a4f63a96923b
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-23 10:57:07 -0300

    load projects

---
+++ src/assets/projects.js
@@ -0,0 +1,17 @@
+import elChef from './images/projects/elChef.png';
+import amadeus from './images/projects/amadeus.png';
+
+const images = [
+    {
+        name: 'The Chef',
+        image: elChef,
+        link: 'https://elchef.vercel.app'
+    },
+    {
+        name: 'AmadeuS',
+        image: amadeus,
+        link: 'https://amadeus.vercel.app'
+    }
+]
+
+export default images;
