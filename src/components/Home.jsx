3c6c84801d59697874aa4e02c5196b3e47bb9bd9
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-23 11:02:24 -0300

    component Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './Nav.jsx';
 import Footer from './Footer.jsx';
+import avatar from '../assets/images/avatar/avatar.png';
 import '../styles/Home.scss';
 
 export default function Home() {
@@ -8,7 +9,15 @@ export default function Home() {
         <>
             <Nav />
             <div className='home'>
-                Home
+                <div className='avatar'>
+                    <a href='https://www.linkedin.com/in/cristian-baronetto' target='_blank' rel='noreferrer'>
+                        <img src={avatar} alt='Cristian Baronetto' />
+                    </a>
+                </div>
+                <div className='text'>
+                    <h1>Cristian Baronetto</h1>
+                    <h1>Full Stack Developer</h1>
+                </div>
             </div>
             <Footer />
         </>
