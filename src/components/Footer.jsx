d0431cd9bd55d49b983d34bf7b148c84419a9905
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-29 10:49:11 -0300

    component Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BsLinkedin, BsGithub, BsWhatsapp } from 'react-icons/bs';
+import { AiOutlineMail, AiOutlineFilePdf } from 'react-icons/ai';
+import cv from '../assets/utils/cv.pdf';
 import '../styles/Footer.scss';
 
 export default function Footer() {
     return (
         <div className='footer'>
-            Footer
+            <a href='https://www.linkedin.com/in/cristian-baronetto' target='_blank' rel='noreferrer'><BsLinkedin size={25} /></a>
+            <a href='https://github.com/Cristian-M-B' target='_blank' rel='noreferrer'><BsGithub size={25} /></a>
+            <a href='https://api.whatsapp.com/send?phone=5491168071990' target='_blank' rel='noreferrer'><BsWhatsapp size={25} /></a>
+            <a href='mailto:crisbaronetto@hotmail.com'><AiOutlineMail size={30} /></a>
+            <a href={cv} download='Cristian Baronetto - Full Stack Developer - CV'><AiOutlineFilePdf size={30} /></a>
         </div>
     )
 }
