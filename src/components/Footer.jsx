fe15c9579c4e30412e2e6804332af644f39c75cd
Cristian-M-B <crisbaronetto@hotmail.com>
2022-01-03 15:52:10 -0300

    update cv

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
             <a href='https://github.com/Cristian-M-B' target='_blank' rel='noreferrer'><BsGithub size={25} /></a>
             <a href='https://api.whatsapp.com/send?phone=5491168071990' target='_blank' rel='noreferrer'><BsWhatsapp size={25} /></a>
             <a href='mailto:crisbaronetto@hotmail.com'><AiOutlineMail size={30} /></a>
-            <a href='https://drive.google.com/file/d/1WWwEQpfJKe2cxvW3rONFVItDo69t-cdl/view' target='_blank' rel='noreferrer'><AiOutlineFilePdf size={30} /></a>
+            <a href='https://drive.google.com/file/d/1RxU-PS31Dnl6xrYCbIyAXI9s51hlbREu/view' target='_blank' rel='noreferrer'><AiOutlineFilePdf size={30} /></a>
         </div>
     )
 }
