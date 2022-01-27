888892bc79abbcb755a7378b61895f6468dbbc8b
Cristian-M-B <crisbaronetto@hotmail.com>
2022-01-27 12:00:42 -0300

    corrections

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Nav from './Nav.jsx';
 import Footer from './Footer.jsx';
 import MediumCard from './MediumCard.jsx';
-import images from '../data/projects.js';
+import projects from '../data/projects.js';
 import '../styles/Projects.scss';
 
 export default function Projects() {
     return (
         <>
             <Nav />
+            <h2 className='title'>Projects</h2>
             <div className='projects'>
                 <div className='images'>
-                    {images?.map(project => {
+                    {projects?.map(project => {
                         return <MediumCard key={project?.name} image={project?.image} name={project?.name} link={project?.link} />
                     })}
                 </div>
