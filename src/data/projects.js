1cb17ef4eb6141075d0ac559b48f43b881a132e8
Cristian-M-B <crisbaronetto@hotmail.com>
2022-01-27 12:01:32 -0300

    add project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,8 @@
 import elChef from '../assets/images/projects/elChef.png';
 import amadeus from '../assets/images/projects/amadeus.png';
+import pokeMania from '../assets/images/projects/pokeMania.png';
 
-const images = [
+const projects = [
     {
         name: 'The Chef',
         image: elChef,
@@ -11,7 +12,12 @@ const images = [
         name: 'AmadeuS',
         image: amadeus,
         link: 'https://amadeus.vercel.app'
+    },
+    {
+        name: 'PokeMania',
+        image: pokeMania,
+        link: 'https://poke-mania.vercel.app'
     }
 ]
 
-export default images;
+export default projects;
